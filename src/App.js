113306b9da7baad096f6d690297b264fbd4cb94c
talhazi <talhazi114@gmail.com>
2021-12-23 21:36:31 +0200

    init admin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useAuth } from '@frontegg/react'
+import { AdminPortal } from '@frontegg/react'
+import { Card, Button } from 'react-bootstrap';
+
+
+const handleClick = () => {
+  AdminPortal.show();
+};
+
 
 function App() {
+  const { user, isAuthenticated } = useAuth();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {isAuthenticated && (
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={user.profilePictureUrl} />
+          <Card.Body>
+            <Card.Title>{user.name}</Card.Title>
+            <Button variant="primary" onClick={handleClick}>Settings</Button>
+          </Card.Body>
+        </Card>
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+import { FronteggProvider } from '@frontegg/react';
+import { BrowserRouter } from 'react-router-dom';
+
+
+const contextOptions = {
+  baseUrl: 'https://app-4gaemze08wlj.frontegg.com',
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+     <FronteggProvider contextOptions={contextOptions}>
+        <App />
+     </FronteggProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
